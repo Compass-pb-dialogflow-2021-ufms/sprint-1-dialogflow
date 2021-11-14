6223d6a441d019a59ee2609910dd76d58b8703c6
Tiago Fugishima <tiago.fugishima@gmail.com>
2021-11-14 20:36:08 -0300

    Commit 3 - Implementado os filtros de pesquisa (Titulo e Autor).

--- src/app.js
+++ src/app.js
@@ -10,7 +10,16 @@ app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
 
 app.get('/', (req, res) => {
-    res.render('home.html', { resultado: '' })
+    res.render('home.html', {
+          titles        : ''
+        , authors       : ''
+        , descriptions  : ''
+        , ranks         : ''
+        , num           : ''
+        , msgmTitulo    : ''
+        , msgmAutor     : ''
+    })
+
 })
 
 app.get('/resultado', async (req, res) => {
@@ -20,19 +29,74 @@ app.get('/resultado', async (req, res) => {
         const autor = req.query.autor
 
         const {data} = await API.get(`/${genero}.json?api-key=${API.defaults.key}`)
+        const booksData = data.results.books
+
+        const books = []
+        let msgmAutor = ''
+        let msgmTitulo = ''
+
+        if (autor) {
+            for (let x = 0; x < booksData.length; x++) {
+                if (booksData[x].author == autor) {
+                    books.push(booksData[x])
+                }
+            }
+            if (books.length == 0) {
+                msgmAutor = 'Autor nao encontrado !'
+            }
+        }
+
+        if (titulo) {
+            if (books.length != 0) {
+                const booksAux = []
+                for (let x = 0; x < books.length; x++) {
+                    if (books[x].title == titulo) {
+                        booksAux.push(books[x])
+                    }
+                }
+
+                if(booksAux.length != 0) {
+                    books.length = 0
+                    books.push(booksAux[0])
+                }
+            } else {
+                for (let x = 0; x < booksData.length; x++) {
+                    if (booksData[x].title == titulo) {
+                        books.push(booksData[x])
+                    }
+                }
+            }
+
+            if (books.length != 1) {
+                msgmTitulo = 'Titulo nao encontrado !'
+            }
+        }
 
         const titles = []
         const authors = []
         const descriptions = []
         const ranks = []
-        for (let x = 0; x < data.results.books.length; x++) {
-            titles[x] = data.results.books[x].title
-            authors[x] = data.results.books[x].author
-            descriptions[x] = data.results.books[x].description
-            ranks[x] = data.results.books[x].rank
+        if (books.length == 0) {
+            for (let x = 0; x < booksData.length; x++) {
+                books.push(booksData[x])
+            }
+        }
+        for (let x = 0; x < books.length; x++) {
+            titles[x] = books[x].title
+            authors[x] = books[x].author
+            descriptions[x] = books[x].description
+            ranks[x] = books[x].rank
         }
 
-        res.render('home.html', { titles: titles, authors: authors, descriptions: descriptions, ranks: ranks })
+        res.render('home.html', {
+              titles        : titles
+            , authors       : authors
+            , descriptions  : descriptions
+            , ranks         : ranks
+            , num           : books.length
+            , msgmTitulo    : msgmTitulo
+            , msgmAutor     : msgmAutor
+        })
     } catch (e) {
 
     }
